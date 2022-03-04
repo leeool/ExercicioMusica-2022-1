dd3b7dde6a5b3f30d4994b60938e7161da3f7f0f
anoddityleol <lgonsalez02@gmail.com>
2022-03-04 13:20:32 -0300

    finaliza

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Please Please Please Let Me Get What I Want - The Smiths");
+
+Console.WriteLine("Good times for a change");
+Console.WriteLine("See, the luck I've had");
+Console.WriteLine("Can make a good man");
+Console.WriteLine("Turn bad");
+
+Console.WriteLine(" ");
+
+Console.WriteLine("So please, please, please");
+Console.WriteLine("Let me, let me, let me");
+Console.WriteLine("Let me get what I want");
+Console.WriteLine("This time");
+
+Console.WriteLine(" ");
+
+Console.WriteLine("Haven't had a dream in a long time");
+Console.WriteLine("See, the life I've had");
+Console.WriteLine("Can make a good man bad");
+
+Console.WriteLine(" ");
+
+Console.WriteLine("So for once in my life");
+Console.WriteLine("Let me get what I want");
+Console.WriteLine("Lord knows, it would be the first time");
+Console.WriteLine("Lord knows, it would be the first time");
